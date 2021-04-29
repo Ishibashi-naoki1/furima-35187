608fb1eecbc2e30e9a6e31fb8bcd18a564780e83
Ishibashi-naoki1 <81144617+Ishibashi-naoki1@users.noreply.github.com>
2021-04-29 15:53:40 +0900

    訂正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   before_action :set_item, except: [:index, :new, :create]
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.all
   end
 
   def new
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(items_params)
     if @item.save
-     redirect_to item_path(@item)
+     redirect_to root_path(@item)
     else
       render :new
     end

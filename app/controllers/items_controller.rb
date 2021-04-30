9435bb5f27b67b1256e90740b140aeb59a53173a
Ishibashi-naoki1 <81144617+Ishibashi-naoki1@users.noreply.github.com>
2021-04-30 14:27:50 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,10 +13,8 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(items_params)
 
-    if @item.valid
-       @item.save
+    if  @item.save
        redirect_to root_path
-
     else
       render :new
     end

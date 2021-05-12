320eb37f175fec8f7bce6cc821073e61b012a885
Ishibashi-naoki1 <81144617+Ishibashi-naoki1@users.noreply.github.com>
2021-05-12 21:18:21 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :order
+  has_many :message, dependent: :destroy
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
